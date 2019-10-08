1fd1baf2609ad422ed597ebd5b75166a582de018
Borislav Damyanov <boris.damyanov@gmail.com>
2019-10-08 10:08:32 +0200

    First Commit

---
+++ src/main/java/com/boris/jenkinsjobs/FirstProject.java
@@ -0,0 +1,7 @@
+package com.boris.jenkinsjobs;
+
+public class FirstProject {
+  public static void main(String[] args) {
+    System.out.println("This is the first test project");
+  }
+}
---
+++ src/main/java/com/boris/jenkinsjobs/JenkinsJobsApplication.java
@@ -0,0 +1,13 @@
+package com.boris.jenkinsjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsJobsApplication.class, args);
+    }
+
+}
